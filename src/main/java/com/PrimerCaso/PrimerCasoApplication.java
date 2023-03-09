3f01de1adf1fa7a22979eb0acd78dd5248f3deed
María José <Mara Jos@DESKTOP-NV09F87>
2023-03-09 12:26:28 -0600

    primer commit

---
+++ src/main/java/com/PrimerCaso/PrimerCasoApplication.java
@@ -0,0 +1,13 @@
+package com.PrimerCaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerCasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerCasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PrimerCaso/PrimerCasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrimerCaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerCasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
